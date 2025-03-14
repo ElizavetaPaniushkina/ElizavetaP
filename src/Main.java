5f38cc24e80d8ea174afad6caf5b41dcd7a495bb
ElizavetaPaniushkina <s33591@pjwstk.edu.pl>
2025-03-14 15:21:14 +0100

    123

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,72 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int[] arr1 = {1, 2, 3, 4, 5, 6};
+        int[] arr2 = filterEvenIndexOddValue(arr1);
+        printArray(arr2);
+        int[] arr3 ={3, 3, 3, 2, 2};
+        int[] arr4 = findDominant(arr3);
+        printArray(arr4);
+        int[] arr5 = {1, 2, 3, 4, 5};
+        int[] arr6 = rotateArray(arr5, 2);
+        printArray(arr6);
     }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int[] filterEvenIndexOddValue(int[] array){
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0 && array[i] % 2 != 0) {
+                list.add(array[i]);
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+    public static int[] findDominant(int[] arr) {
+        int candidate = -1;
+        int count = 0;
+
+        for (int num : arr) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (candidate == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        count = 0;
+        for (int num : arr) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+        return new int[]{(count > arr.length / 2) ? candidate : -1};
+    }
+
+
+    public static int[] rotateArray(int[] array, int positions) {
+        int[] result = new int[array.length];
+        int n=0;
+        for (int i = positions + 1; i < array.length; i++) {
+            result[n] = array[i];
+            n++;
+        }
+        return result;
+    }
+
 }
